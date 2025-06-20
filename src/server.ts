a133c64c49cc54fed911a0faae2ff646b8521724
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-06-20 20:13:25 +0600

    added post in env file

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 require('dotenv').config();
 
 let server: Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 async function main() {
     try {
         await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rz0kihv.mongodb.net/Library-Management?retryWrites=true&w=majority&appName=Cluster0`)

1cc24f654e5062f3d033080a426f31604146be60
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-07-04 15:54:50 +0600

    fix the cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express"
 import { booksRouter } from "./app/controllers/books.controller"
 import { borrowRouter } from "./app/controllers/borrow.controller"
-var cors = require('cors')
+import cors from 'cors';
 
 export const app: Application = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://book-lovers-alpha.vercel.app", "http://localhost:5173"],
+    credentials: true
+}))
 app.use("/api/books", booksRouter)
 app.use("/api/borrow", borrowRouter)
 
